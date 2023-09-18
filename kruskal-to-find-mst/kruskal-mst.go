a820473356ced87b4c3ba100235177dd9a2a2f92
Tiago Avila <tiago.avila09@gmail.com>
2023-09-18 09:53:40 -0300

    fixed errors

--- kruskal-to-find-mst/kruskal-mst.go
+++ kruskal-to-find-mst/kruskal-mst.go
@@ -3,8 +3,11 @@ package kruskaltofindmst
 import "top25algorithms/weightedundirectedgraph"
 
 func KruskalMST(graph *weightedundirectedgraph.WeightedUndirectedGraph) []weightedundirectedgraph.Edge {
+	mst := make([]weightedundirectedgraph.Edge, 0)
+
 	// Sort all the edges in non-decreasing order of their weight.
 	uniqueEdges := graph.GetUniqueEdges()
 	uniqueEdges = graph.SortEdgesByWeight(uniqueEdges)
 
+	return mst
 }
