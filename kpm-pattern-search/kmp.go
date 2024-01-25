f261cc35f2424c3ade1cb6d3f73ed566030c3205
Tiago Avila <tiago.avila09@gmail.com>
2024-01-25 11:48:41 -0300

    KMP

--- kpm-pattern-search/kmp.go
+++ kpm-pattern-search/kmp.go
@@ -1,9 +1,34 @@
 package kpmpatternsearch
 
-func FindSubStrings(text string, pattern string) []int {
-	return make([]int, 0)
+func FindIndexOfSubStrings(text string, pattern string) []int {
+	matchesArray := make([]int, 0)
+
+	textLength, patternLength := len(text), len(pattern)
+	textIndex, patternIndex := 0, 0
+	lpsArray := ComputeLPSArray(pattern)
+
+	for textIndex < textLength {
+		if text[textIndex] == pattern[patternIndex] {
+			textIndex++
+			patternIndex++
+
+			if patternIndex == patternLength {
+				matchesArray = append(matchesArray, textIndex-patternIndex)
+				patternIndex = lpsArray[patternIndex-1]
+			}
+		} else {
+			if patternIndex != 0 {
+				patternIndex = lpsArray[patternIndex-1]
+			} else {
+				textIndex++
+			}
+		}
+	}
+
+	return matchesArray
 }
 
+// https://www.youtube.com/watch?v=4jY57Ehc14Y&t=551s
 func ComputeLPSArray(pattern string) []int {
 	prefixLength, i := 0, 1
 	lpsArray := make([]int, len(pattern))
--- kpm-pattern-search/kmp_test.go
+++ kpm-pattern-search/kmp_test.go
@@ -19,11 +19,37 @@ func TestComputeLPSArray(t *testing.T) {
 		{"Test2FromAbdulBariChannel", "ABCDEABFABC", []int{0, 0, 0, 0, 0, 1, 2, 0, 1, 2, 3}},
 		{"Test3FromAbdulBariChannel", "AABCADAABE", []int{0, 1, 0, 0, 1, 0, 1, 2, 3, 0}},
 		{"Test4FromAbdulBariChannel", "AAAABAACD", []int{0, 1, 2, 3, 0, 1, 2, 0, 0}},
+		{"Test5FromAbdulBariChannel", "ABABD", []int{0, 0, 1, 2, 0}},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := ComputeLPSArray(tt.pattern)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ComputeLPSArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestKMPPatternSearch(t *testing.T) {
+	tests := []struct {
+		name    string
+		text    string
+		pattern string
+		want    []int
+	}{
+		{"Test1", "ABABCABCABABABD", "ABABD", []int{10}},
+		{"Test2", "ABABABABCABABABD", "ABABD", []int{11}},
+		{"Test3", "AABAACAADAABAABA", "AABA", []int{0, 9, 12}},
+		{"Test4", "ABCABCABCABC", "ABC", []int{0, 3, 6, 9}},
+		{"Test5", "GEEKSFORGEEKS", "GEEKS", []int{0, 8}},
+		{"Test5", "AAAAAA", "AA", []int{0, 1, 2, 3, 4}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := FindIndexOfSubStrings(tt.text, tt.pattern)
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("ComputeLPSArray() = %v, want %v", got, tt.want)
 			}
